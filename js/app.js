0e13737769d4ef397a78aa742e73b141d33bdb25
Yannik Hangst <yanso1@posteo.de>
2022-05-11 19:46:53 +0200

    add first toggle options

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
-// Bookmark
+//Change color of bookmark
+const bookmark = document.querySelector('.bookmark');
 
-const bookmark = document.querySelector(".bookmark");
-
-bookmark.addEventListener("click", () => {
-  bookmark.classList.toggle("filled");
+bookmark.addEventListener('click', () => {
+  bookmark.classList.toggle('filled');
 });
 
-// Answer
-const button = document.querySelector(".card__button");
-const answer = document.querySelector(".answer");
+//Show answer
+const button = document.querySelector('.card__button');
+const answer = document.querySelector('.hidden');
 
-button.addEventListener("click", () => {
-  answer.classList.toggle("answer");
-  button.textContent = "Hide Answer";
+button.addEventListener('click', () => {
+  if (button.textContent === 'Show Answer') {
+    button.textContent = 'Hide Answer';
+  } else {
+    button.textContent = 'Show Answer';
+  }
+  answer.classList.toggle('.hidden');
+  //this works but not chnages back afterwards
 });
