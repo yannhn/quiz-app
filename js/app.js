92b2b7de5513e2380f1cec41be554e9c84d6a49e
Yannik Hangst <yanso1@posteo.de>
2022-05-13 12:43:29 +0200

    fix button and answer

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 import { bookmarkFunction } from './bookmark.js';
+import { toggleAnswer } from './toggleAnswer.js';
 
 // import { toggleAnswer } from './js/toggleAnswer';
 
 //Change color of bookmark
 const bookmarks = document.querySelectorAll('[data-js="bookmark_js"]');
 bookmarks.forEach(bookmarkFunction);
+
+// toggle button and answer
+const buttons = document.querySelectorAll('[data-js="button_js"]');
+buttons.forEach(toggleAnswer);
