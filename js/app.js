86e6b042a10739bcce87cd4983766f9ee398eceb
Yannik Hangst <yanso1@posteo.de>
2022-05-15 23:36:27 +0200

    finish toggle and start counter

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 import { bookmarkFunction } from './bookmark.js';
 import { toggleAnswer } from './toggleAnswer.js';
+import './toggleSites.js';
 
 // import { toggleAnswer } from './js/toggleAnswer';
 
@@ -10,3 +11,23 @@ bookmarks.forEach(bookmarkFunction);
 // toggle button and answer
 const buttons = document.querySelectorAll('[data-js="button_js"]');
 buttons.forEach(toggleAnswer);
+
+// count strings
+
+let textAreaQuestion = document.querySelector('.input__question');
+let characterCounterQuestion = document.querySelector('.counter__question');
+
+const countQuestion = () => {
+  let counter = textAreaQuestion.value.length;
+  characterCounterQuestion.textContent = counter;
+};
+textAreaQuestion.addEventListener('input', countQuestion);
+
+let textAreaAnswer = document.querySelector('.input__answer');
+let characterCounterAnswer = document.querySelector('.counter__answer');
+
+const countAnswer = () => {
+  let counter = textAreaAnswer.value.length;
+  characterCounterAnswer.textContent = counter;
+};
+textAreaAnswer.addEventListener('input', countAnswer);
